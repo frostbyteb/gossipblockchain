06f7f43ee6d7448a087f88b338b689942e7c9787
Duman <108366928+dumanzhusupov@users.noreply.github.com>
2024-02-13 00:05:31 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById("registration-form").addEventListener("submit", function(event) {
+    // Halt! We must prevent the customary rituals of form submission!
     event.preventDefault(); 
-    console.log("Registration submitted.");
+    // Ahoy! The registration scroll hath been unfurled, ready to embark on its journey!
+    console.log("Hark! The registration scroll hath been unfurled.");
 });
-
